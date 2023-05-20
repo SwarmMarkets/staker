5d86c225471e4ca953bb2a0aac0aff54aa949e0e
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-20 14:08:32 +0300

    add: contracts refs

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,14 +3,19 @@ import { UniswapV3Staker } from '../typechain';
 import hre, { ethers } from 'hardhat';
 import { ContractFactory } from 'ethers';
 
-
+const UniswapV3Factory = '0x1F98431c8aD98523631AE4a59f267346ea31F984' // All networks
+const NonfungiblePositionManager = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88' // All networks
+const maxIncentiveStartLeadTime = 1
+const maxIncentiveDuration = 1
 
 
 async function main() {
   const { ethers } = hre;
 
   const UniswapV3Staker: ContractFactory = await ethers.getContractFactory('UniswapV3Staker');
-  const uniswapV3Staker: UniswapV3Staker = await UniswapV3Staker.deploy() as UniswapV3Staker;
+  const uniswapV3Staker: UniswapV3Staker = await UniswapV3Staker.deploy(
+    UniswapV3Factory, NonfungiblePositionManager, maxIncentiveStartLeadTime, maxIncentiveDuration
+  ) as UniswapV3Staker;
   await uniswapV3Staker.deployed();
 
   console.log(`Deployed to ${uniswapV3Staker.address}`);
