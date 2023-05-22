55e67bca3e4d0b6abd8caa6bdeb7105e2c6d8ea3
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-22 10:57:22 +0300

    cahnges: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,19 @@ import { HardhatUserConfig } from 'hardhat/config'
 import { SolcUserConfig } from 'hardhat/types'
 import 'solidity-coverage'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Ensure that we have all the environment variables we need.
+const mnemonic = process.env.MNEMONIC || '';
+const privateKey = process.env.PRIVATE_KEY || '';
+
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || '';
+const polyscanApiKey = process.env.POLYSCAN_API_KEY || '';
+
+const infuraApiKey = process.env.INFURA_API_KEY;
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+
 const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
   version: '0.7.6',
   settings: {
@@ -37,36 +50,73 @@ if (process.env.RUN_COVERAGE == '1') {
 }
 
 const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: false,
+      blockGasLimit: 20000000,
+      throwOnCallFailures: false,
+      chainId: 31337,
+      initialBaseFeePerGas: 0,
+      accounts: {
+        mnemonic,
+        accountsBalance: '1000000000000000000000',
+      },
+    },
+    ganache: {
+      url: 'http://localhost:8545', // Localhost (default: none)
+      chainId: 1337,
+      blockGasLimit: 20000000,
+      timeout: 200000,
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`,
+      chainId: 1,
+      accounts: { mnemonic },
+      // accounts: {
+      //   mnemonic,
+      // },
+      gas: 2100000,
+      gasPrice: 45000000000, // 45
     },
     polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+      chainId: 137,
+      accounts: { mnemonic },
+      gas: 2100000,
+      gasPrice: 45000000000, // 45
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${alchemyApiKey}`,
+      chainId: 80001,
+      accounts: { mnemonic },
+      gas: 2100000,
+      gasPrice: 45000000000, // 45
+      gasMultiplier: 2,
     },
   },
+  paths: {
+    artifacts: './artifacts',
+    cache: './cache',
+    sources: './contracts',
+    tests: './test',
+  },
+
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
   },
   contractSizer: {
     alphaSort: false,
     disambiguatePaths: true,
     runOnCompile: false,
   },
-}
+  etherscan: {
+    apiKey: etherscanApiKey || polyscanApiKey,
+  },
+};
 
-if (process.env.ETHERSCAN_API_KEY) {
-  config.etherscan = {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  }
-}
 
 export default config
